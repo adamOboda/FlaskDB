1eb4faa85bab279226fd38737fa7199746bff68d
Adam <adam.oboda1998@gmail.com>
2022-06-21 21:25:59 +0200

    changes

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask.templating import render_template
+
+
 app = Flask(__name__,)
+app.debug = True
+
 
 
 @app.route('/')
 def index():
     return render_template("/index.html")
 
+@app.route("/about)
+def about():
+    return render_template("about.html")
+    
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
